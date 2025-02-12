97113ef78a1930be5f17271726615c1c926ee15e
Pedro <pedroaugusto417@yahoo.com>
2025-02-11 21:54:06 -0300

    Updade exexercicios

---
+++ src/main/java/list/listajavacondicionais/Java02/NomeSexoCivil.java
@@ -0,0 +1,28 @@
+package listajavacondicionais.Java02;
+
+import java.util.Scanner;
+
+public class NomeSexoCivil {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int years = 0;
+
+        System.out.printf("Digite seu Nome: ");
+        String nome = sc.nextLine();
+        System.out.printf("Digite seu Sexo [M ou F]: ");
+        String sexo = sc.nextLine();
+        System.out.printf("Digite seu Estado Civil: ");
+        String estadoCivil = sc.nextLine();
+
+        String sexoMaiusculo = sexo.toUpperCase();
+        String estadoCivilMaiusculo = estadoCivil.toUpperCase();
+
+
+        if(sexoMaiusculo.equals("F") && estadoCivilMaiusculo.equals("CASADA")){
+            System.out.printf("Quanto tempo de casada em anos: ");
+            years = sc.nextInt();
+        }
+        System.out.println(nome+" você é do sexo "+sexoMaiusculo+" e é "+estadoCivilMaiusculo+" a "+years+" ano(s)");
+
+    }
+}
---
+++ src/main/java/list/listajavacondicionais/Java09/PesoIdeal.java
@@ -0,0 +1,25 @@
+package listajavacondicionais.Java09;
+import java.util.Scanner;
+public class PesoIdeal {
+    public static void main (String[]args){
+        Scanner sc = new Scanner (System.in);
+
+        System.out.printf("DIGITE  A SUA ALTURA: ");
+        double altura = sc.nextDouble();
+        System.out.printf("QUAL É O SEU SEXO [F] OU [M]: ");
+        String sexo = sc.next();
+
+        String uppercaseSexo = sexo.toUpperCase();
+        double peso = 0;
+
+        if(uppercaseSexo.equals("M")){
+            peso = (72.7 * altura) - 58;
+            System.out.printf("Seu peso ideal = %.1f",peso);
+        }
+        else if(uppercaseSexo.equals("F")){
+            peso = (62.1 * altura) - 44.7;
+            System.out.printf("Seu peso ideal = %.1f ",peso);
+        }
+
+    }
+}
