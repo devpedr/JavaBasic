31cdac77f41ef517faf9a4f3c4e37ca0299d5c3f
Pedro <pedroaugusto417@yahoo.com>
2025-03-27 21:23:22 -0300

    Exercise OOP

---
+++ src/exercice/pOO/produtoEstoque/Product.java
@@ -0,0 +1,20 @@
+package src.exercice.pOO.produtoEstoque;
+
+public class Product {
+    public String name;
+    public double price;
+    public int quantiy;
+
+    public double TotalValueInStock (){
+        return price * quantiy;
+    }
+    public void AddProducts(int quantiy){
+        this.quantiy += quantiy;
+    }
+    public void RemoveProducts(int quantiy){
+        this.quantiy -= quantiy;
+    }
+    public String toString(){
+        return "Product data: " + name + ", " + "$" + String.format("%.2f", price) + ", " + quantiy + " units, Total: $" + String.format("%.2f",TotalValueInStock());
+    }
+}
---
+++ src/exercice/pOO/produtoEstoque/main.java
@@ -0,0 +1,36 @@
+package src.exercice.pOO.produtoEstoque;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Product product = new Product();
+
+        System.out.println("Enter product data: ");
+        System.out.printf("Name : ");
+        product.name = sc.nextLine();
+        System.out.printf("Price : ");
+        product.price = sc.nextDouble();
+        System.out.printf("Quantity : ");
+        product.quantiy = sc.nextInt();
+        System.out.println(product);//toString
+
+        System.out.print("\nEnter the number of products to be added in stock: ");
+        int quantity = sc.nextInt();
+        product.AddProducts(quantity);
+        System.out.println("Update data : " + product);// troString
+
+        System.out.print("\nEnter the number of products to be removed from stock: ");
+        quantity = sc.nextInt();
+        product.RemoveProducts(quantity);
+        System.out.print("Update data : " + product);//toString
+
+        sc.close();
+
+
+
+    }
+}
